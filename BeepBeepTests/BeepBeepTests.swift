661ebcecbc4942291a9c55fdbb1f666bf7e1c3cd
JiWon Lee <jiwon.swdev@gmail.com>
2021-05-06 22:26:40 +0900

    feat: create Project

---
+++ BeepBeepTests/BeepBeepTests.swift
@@ -0,0 +1,33 @@
+//
+//  BeepBeepTests.swift
+//  BeepBeepTests
+//
+//  Created by 이지원 on 2021/05/06.
+//
+
+import XCTest
+@testable import BeepBeep
+
+class BeepBeepTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ Podfile
@@ -0,0 +1,24 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '13.0'
+
+target 'BeepBeep' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for BeepBeep
+  pod 'RxSwift', '6.1.0'
+  pod 'RxCocoa', '6.1.0'
+  pod 'SwiftLint'
+  pod 'SnapKit', '~> 5.0.0'
+  pod 'Then'
+
+  target 'BeepBeepTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'BeepBeepUITests' do
+    # Pods for testing
+  end
+
+end
