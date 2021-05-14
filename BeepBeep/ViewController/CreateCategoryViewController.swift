6350e5bb38b2638f643a9dd9e7c7b2f35d10db98
JiWon Lee <jiwon.swdev@gmail.com>
2021-05-14 22:08:18 +0900

    [#8] feat: mainVC to createVC Screen transition

---
+++ BeepBeep/ViewController/CreateCategoryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CreateCategoryViewController.swift
+//  BeepBeep
+//
+//  Created by 이지원 on 2021/05/14.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class CreateCategoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
