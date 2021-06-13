94394e371c614a94e9af7233ccb8aba0f5e2af48
JiWon Lee <jiwon.swdev@gmail.com>
2021-06-13 22:30:48 +0900

    [#7] view: create Record View Controller

---
+++ BeepBeep/ViewController/RecordViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RecordViewController.swift
+//  BeepBeep
+//
+//  Created by 이지원 on 2021/06/13.
+//
+
+import UIKit
+
+class RecordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+}
