acaa79475973b4bffa9c59869215919190afc185
JiWon Lee <jiwon.swdev@gmail.com>
2021-06-13 20:54:41 +0900

    [#6] view: configure Item Detail Scene

--- BeepBeep/ViewController/ItemDetailViewController.swift
+++ BeepBeep/ViewController/ItemDetailViewController.swift
@@ -6,8 +6,39 @@
 //
 
 import UIKit
+import SnapKit
+import Then
+import RxSwift
+import RxCocoa
 
 class ItemDetailViewController: UIViewController {
+
+    // MARK: - Properties
+    let disposeBag = DisposeBag()
+
+    // MARK: - View Properties
+    private let titleLabel = UILabel().then {
+        $0.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+        $0.text = "What is your favorite?"
+        $0.textColor = .label
+    }
+
+    private let answerTextView = UITextView().then {
+        $0.font = UIFont.preferredFont(forTextStyle: .body)
+        $0.isEditable = false
+        $0.backgroundColor = UIColor(named: "BeepGray")
+        $0.textColor = .label
+        $0.layer.cornerRadius = 15
+        $0.clipsToBounds = true
+    }
+
+    private let recordTitleLabel = UILabel().then {
+        $0.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+        $0.text = "Detail Blah"
+        $0.textColor = .label
+    }
+
+    private lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,6 +46,12 @@ class ItemDetailViewController: UIViewController {
         view.backgroundColor = .systemBackground
 
         configureNavigation()
+        configureViews()
+        setCollectionView()
+        setTitleLabelConstraints()
+        setAnswerTextViewConstraints()
+        setRecordTitleLabelConstraints()
+        setCollectionViewConstraints()
     }
 }
 
@@ -63,6 +100,51 @@ private extension ItemDetailViewController {
             alert.addAction(cancelAction)
 
             self.present(alert, animated: true, completion: nil)
+        }
+    }
+
+    func configureViews() {
+        view.addSubview(titleLabel)
+        view.addSubview(answerTextView)
+        view.addSubview(recordTitleLabel)
+        view.addSubview(collectionView)
+    }
+
+    func setCollectionView() {
+        collectionView.backgroundColor = .none
+        collectionView.register(CollectionsCell.self, forCellWithReuseIdentifier: CollectionsCell.identifier)
+    }
+    func setTitleLabelConstraints() {
+        titleLabel.snp.makeConstraints {
+            $0.leading.equalTo(view.safeAreaLayoutGuide.snp.leading).offset(32)
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(32)
+            $0.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing).offset(-32)
+        }
+    }
+
+    func setAnswerTextViewConstraints() {
+        answerTextView.snp.makeConstraints {
+            $0.leading.equalTo(titleLabel.snp.leading)
+            $0.top.equalTo(titleLabel.snp.bottom).offset(32)
+            $0.trailing.equalTo(titleLabel.snp.trailing)
+            $0.height.equalTo(view.safeAreaLayoutGuide.snp.height).multipliedBy(0.3)
+        }
+    }
+
+    func setRecordTitleLabelConstraints() {
+        recordTitleLabel.snp.makeConstraints {
+            $0.leading.equalTo(titleLabel.snp.leading)
+            $0.top.equalTo(answerTextView.snp.bottom).offset(32)
+            $0.trailing.equalTo(titleLabel.snp.trailing)
+        }
+    }
+
+    func setCollectionViewConstraints() {
+        collectionView.snp.makeConstraints {
+            $0.leading.equalTo(titleLabel.snp.leading)
+            $0.top.equalTo(recordTitleLabel.snp.bottom).offset(32)
+            $0.trailing.equalTo(titleLabel.snp.trailing)
+            $0.bottom.equalToSuperview().offset(-32)
         }
     }
 }
