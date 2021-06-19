c4166716234184520cd4de32fa6cce0e532a0cee
JiWon Lee <jiwon.swdev@gmail.com>
2021-06-19 22:15:25 +0900

    [#7] view: tapped record button, move to panel half

--- BeepBeep/ViewController/RecordViewController.swift
+++ BeepBeep/ViewController/RecordViewController.swift
@@ -6,10 +6,25 @@
 //
 
 import UIKit
+import SnapKit
+import Then
+import RxSwift
+import RxCocoa
+import FloatingPanel
 
 class RecordViewController: UIViewController {
 
+    // MARK: - Properties
+    private let disposeBag = DisposeBag()
+
     // MARK: - View Properties
+    let recordView = UIView().then {
+        $0.layer.cornerRadius = 33
+        $0.layer.borderWidth = 3
+        $0.layer.borderColor = UIColor.secondaryLabel.cgColor
+        $0.layer.masksToBounds = true
+    }
+
     let recordButton = RecordButton()
 
     override func viewDidLoad() {
@@ -17,19 +32,41 @@ class RecordViewController: UIViewController {
 
         view.backgroundColor = .systemBackground
         configureViews()
+        setRecordViewConstraints()
         setRecordButtonConstraints()
+        bindInput()
     }
 }
 
 private extension RecordViewController {
     func configureViews() {
-        view.addSubview(recordButton)
+        recordView.addSubview(recordButton)
+        view.addSubview(recordView)
+    }
+
+    func setRecordViewConstraints() {
+        recordView.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalToSuperview().offset(-32)
+            $0.width.equalTo(66)
+            $0.height.equalTo(recordView.snp.width)
+        }
     }
 
     func setRecordButtonConstraints() {
         recordButton.snp.makeConstraints {
-            $0.centerX.equalToSuperview()
-            $0.centerY.equalToSuperview()
+            $0.center.equalToSuperview()
         }
+    }
+
+    func bindInput() {
+        recordButton.rx.tap
+            .delay(.milliseconds(5), scheduler: MainScheduler.instance)
+            .bind { self.moveToHalf() }
+            .disposed(by: disposeBag)
+    }
+
+    func moveToHalf() {
+        (self.parent as! FloatingPanelController).move(to: .half, animated: true)
     }
 }
